d5aa3dacb984d732942d65647c09d62a9ac5a2db
CodeByBlazej <codebyblazej@gmail.com>
2024-11-20 19:31:41 +0000

    test prepend method

--- main.rb
+++ main.rb
@@ -2,9 +2,15 @@ require_relative "linked_list"
 
 list = LinkedList.new
 
-list.append('dog')
-list.append('cat')
-list.append('parrot')
+# list.append('dog')
+# list.append('cat')
+# list.append('parrot')
+
+list.prepend('dog')
+list.prepend('cat')
+list.prepend('parrot')
+
+list.size
 
 
 
